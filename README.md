422736b9271b3dfb582580098e96277cdd2b7b53
Jain Waldrip <jain.waldrip@puppet.com>
2022-02-09 16:53:28 -0600

    Add patch_k8s_hosts script

---
+++ README.md
@@ -0,0 +1,3 @@
+# CD4PE Utility Scripts
+
+This is just a directory of useful CD4PE installation/provisioning scripts.
---
+++ lib/patch_k8s_hosts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# !!! This must be done on the image in Instruqt !!!
+
+# adding host alias for the primary to the cd4pe and query deployment configurtions
+DOMAIN=$(get_fqdn)
+MASTER_IP=$(ssh -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@puppet '(ifconfig -a)' | grep -oP "(?<=inet )\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | awk '!/127.0.0.1/')
+GITLAB_IP=$(ssh -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@gitlab '(ifconfig -a)' | grep -oP "(?<=inet )\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | awk '!/127.0.0.1/')
+
+read -r PATCH <<EOF
+{"spec":{"template":{"spec":{"hostAliases":[{"ip":"${MASTER_IP}","hostnames":["puppet.${DOMAIN}"]},{"ip":"${GITLAB_IP}","hostnames":["gitlab.${DOMAIN}"]}]}}}}
+EOF
+
+kubectl patch deployment cd4pe -n ${NAMESPACE} --patch "${PATCH}"
+kubectl patch deployment query -n ${NAMESPACE} --patch "${PATCH}"
